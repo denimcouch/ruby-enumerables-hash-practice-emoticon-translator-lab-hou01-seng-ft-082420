e367e47e00d2a11397f2265110b5740a5835146c
Alex Mata <alexmata92@ymail.com>
2020-08-07 19:38:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_english_meaning(file_path, emoticon)
   # code goes here
   emos = load_library(file_path)
   emos.each do |key, value|
-   #binding.pry
+   binding.pry
     if emos[key][:japanese]
       return emos[key]
     end
@@ -30,13 +30,4 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  emos = load_library(file_path)
-  emos.each do |key, value|
-   #binding.pry
-    if emos[key][:english]
-      emos[key][:japanese]
-    end
-    #binding.pry
-  end 
-  #binding.pry 
 end
