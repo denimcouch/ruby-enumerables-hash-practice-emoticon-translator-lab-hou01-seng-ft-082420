20898940344b32d26853c4b0604b710887161867
Alex Mata <alexmata92@ymail.com>
2020-08-07 19:46:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(file_path, emoticon)
     if emos[key][:japanese] == emoticon
        return emos[key]
     end
-    binding.pry
+    #binding.pry
   end 
   #binding.pry 
 end
