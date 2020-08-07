08b675e28925b2db6196c0bfb64b2dc9457b16a6
Alex Mata <alexmata92@ymail.com>
2020-08-07 19:33:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_english_meaning(file_path, emoticon)
   #binding.pry 
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   emos = load_library(file_path)
   emos.each do |key, value|
